10b91a2ecb7b97d782d374360b9250896a5dd012
Jovino <jovribeiro@gmail.com>
2015-02-24 21:53:38 -0800

    handling logout for leialater

--- src/leialater/urls.py
+++ src/leialater/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^dashboard/$', views.dashboard, name='dasbhoard'),   
     url(r'^register/$', views.register, name='register'),      
     url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
    )
 
--- src/leialater/views.py
+++ src/leialater/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.template.context import RequestContext
@@ -59,6 +60,9 @@ def user_login(request):
     else:
         return render(request, 'leialater/login.html', {})
          
-
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/leialater/')
     
 
